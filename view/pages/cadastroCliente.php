44286de9cbbd69cc558b91dfdd6bdc13bac53ddf
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-06 19:45:07 -0300

    Update cadastroCliente.php

--- view/pages/cadastroCliente.php
+++ view/pages/cadastroCliente.php
@@ -19,11 +19,13 @@
                                 <input type="text" placeholder="Nome" name="txtNome" minlength="5" maxlength="80" required>
                                 <input type="text" placeholder="CPF" name="txtCPF" minlength="5" maxlength="11" required>
                             </section>
-                            <section id="endereco">
-                                <p>Informação do endereço:</p>
-                                <input type="text" placeholder="Cep" name="txtCep" minlength="8" maxlength="8" pattern="[0-9]{5}[0-9]{3}"required>
-                                <input type="text" placeholder="UF" name="txtUF" minlength="2" maxlength="2" pattern="[aA-zZ]{2}"required>
-                                <input type="text" placeholder="Numero" name="txtNumero" minlength="1" maxlength="5" required>
+                        </fieldset>
+                        <fieldset>
+                        <section id="endereco">
+                            <p>Informação do endereço:</p>
+                            <input type="text" placeholder="Cep" name="txtCep" minlength="8" maxlength="8" pattern="[0-9]{5}[0-9]{3}" required>
+                                <input type="text" placeholder="UF" name="txtUF" minlength="2" maxlength="2" pattern="[aA-zZ]{2}" required>
+                                <input type="text" placeholder="Numero" name="txtNumero" pattern="[0-9]" required>
                                 <input type="text" placeholder="Cidade" name="txtCidade" minlength="5" maxlength="30" required>
                                 <input type="text" placeholder="Bairro" name="txtBairro" minlength="5" maxlength="30" required>
                                 <input type="text" placeholder="Rua" name="txtRua" minlength="5" maxlength="30" required>
