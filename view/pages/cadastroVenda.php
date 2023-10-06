5bbc7bf288c6bedcc8a29f2acf13a22f9fdb2804
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-06 20:23:38 -0300

    Add

--- view/pages/cadastroVenda.php
+++ view/pages/cadastroVenda.php
@@ -8,7 +8,34 @@
     </head>
     <body>
         <?php require ('../layout/header.php') ?>
-        <main></main>
+        <main>
+            <article>
+                <section>
+                    <form method="post" action="">
+                        <h2>Cadastro venda</h2>
+                        <fieldset>
+                            <p>Informação do cliente:</p>
+                            <select name="txtCliente">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                            <input type="text" placeholder="Pesquisar">
+                        </fieldset>
+                        <fieldset>
+                            <p>Informação do produto:</p>
+                            <select name="txtProduto">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                            <input type="number" placeholder="Quantidade" name="txtQuantidade" required>
+                        </fieldset>
+                        <button type="submit" id="btnSubmit">Enviar</button>
+                    </form>
+                </section>
+            </article>
+        </main>
         <?php require ('../layout/footer.php') ?>
     </body>
 </html>
