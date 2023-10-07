f0b4a14227219310987f9241f8b04f2d1e02c1a6
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-07 16:59:15 -0300

    add

--- view/pages/sobre.php
+++ view/pages/sobre.php
@@ -5,26 +5,45 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sobre</title>
+        <link rel="stylesheet" href="../css/sobre.css">
         <?php require ('../layout/head.php') ?>
     </head>
     <body>
         <?php require ('../layout/header.php') ?>
         <main>
-            <article>
-                <section>
+            <article class="sobre">
+                <section class="quem-somos">
                     <h2>Quem somos</h2>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
                     </p>
                 </section>
-                <section>
-                    <h2>O que foi feito</h2>
-                    <div>
-                        <img src="" alt="Integrante">
-                        <h3>Nome</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
-                        </p>
+                <section class="cada-um">
+                    <h2>O que cada um fez:</h2>
+                    <div class="integrantes">
+                        <div class="pessoa">
+                            <img src="../img/teste.png" alt="Integrante">
+                            <h3>Gustavo Sachetto</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                            </p>
+                        </div>
+                        <div class="pessoa">
+                            <img src="../img/teste.png" alt="Integrante">
+                            <h3>Gustavo Sachetto</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                            </p>
+                        </div>
+                        <div class="pessoa">
+                            <img src="../img/teste.png" alt="Integrante">
+                            <h3>Gustavo Sachetto</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                            </p>
+                        </div>
                     </div>
                 </section>
             </article>
