0db1e5eac55036a4b800ee362104392a7fc23e0b
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-07 10:47:04 -0300

    Add Bootstrap

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Inicio</title>
+
+        <?php require ('view/layout/head.php') ?>
     </head>
     <body>
         <header>
--- view/pages/cadastroCliente.php
+++ view/pages/cadastroCliente.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cadastro cliente</title>
+        <?php require ('../layout/head.php') ?>
     </head>
     <body>
         <?php require ('../layout/header.php') ?>
