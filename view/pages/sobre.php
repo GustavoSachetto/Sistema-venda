df97adfd9d30e8b13129f51a113ed7f8f7ca682f
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-07 10:41:22 -0300

    Update sobre.php

--- view/pages/sobre.php
+++ view/pages/sobre.php
@@ -8,7 +8,26 @@
     </head>
     <body>
         <?php require ('../layout/header.php') ?>
-        <main></main>
+        <main>
+            <article>
+                <section>
+                    <h2>Quem somos</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                    </p>
+                </section>
+                <section>
+                    <h2>O que foi feito</h2>
+                    <div>
+                        <img src="" alt="Integrante">
+                        <h3>Nome</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
+                        </p>
+                    </div>
+                </section>
+            </article>
+        </main>
         <?php require ('../layout/footer.php') ?>
     </body>
 </html>
