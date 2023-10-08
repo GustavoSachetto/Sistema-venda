952cd7617be341f7ba8729aa763c9d312cadf9e8
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-08 20:08:31 -0300

    Update header.php

--- view/layout/header.php
+++ view/layout/header.php
@@ -2,7 +2,7 @@
     <h1>Sistema - venda</h1>
     <nav>
         <a href="../../index.php" role="button" class="btn btn-lg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16"><path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/></svg><span> Inicio</span></a>
-        <a href="../pages/sobre.php" role="button" class="btn btn-lg">Sobre</a>
+        <a href="../pages/sobre.php" role="button" class="btn btn-lg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-person-fill" viewBox="0 0 16 16"><path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z"/></svg><span> Sobre</span></a>
         <div class="btn-group" role="group">
             <!-- Cliente -->
             <div>
