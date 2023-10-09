9eca94f5606b5fde5ed1ecfb2c0e9e62cf6001ef
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-09 11:43:43 -0300

    Update sobre.php

--- view/pages/sobre.php
+++ view/pages/sobre.php
@@ -11,29 +11,7 @@
     <body>
         <?php require ('../layout/header.php') ?>
         <main>
-            <article class="sobre">
-                <section class="quem-somos">
-                    <h2>Quem somos</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae sit aliquid animi error aperiam ad quas, ex incidunt natus corporis saepe repellat tempora consequatur culpa repellendus inventore ipsa magnam.
-                    </p>
-                </section>
-                <section class="foi-feito">
-                    <h2>O que fizemos</h2>
-                    <div class="row row-cols-1 row-cols-md-3 g-4">
-                        <div class="col">
-                            <div class="card h-100">
-                            <img src="../img/teste.png" class="card-img-top" alt="Integrante">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+            <article>
             </article>
         </main>
         <?php require ('../layout/footer.php') ?>
