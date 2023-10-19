6237bab9a1008728882b02e8663a57426cf4862d
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-19 15:57:53 -0300

    Update header.php

--- view/layout/header.php
+++ view/layout/header.php
@@ -1,52 +1,52 @@
 <header>
-            <h1>Sistema - venda</h1>
-            <button id="btn-menu" class="btn-menu">
-                Menu
+    <h1>Sistema - venda</h1>
+    <button id="btn-menu" class="btn-menu">
+        Menu
+    </button>
+    <nav>
+        <a href="../../index.php" role="button" class="btn btn-lg">Inicio</a>
+        <a href="../pages/sobre.php" role="button" class="btn btn-lg">Sobre</a>
+        <div>
+            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
+                Cliente
             </button>
-            <nav>
-                <a href="../../index.php" role="button" class="btn btn-lg">Inicio</a>
-                <a href="../pages/sobre.php" role="button" class="btn btn-lg">Sobre</a>
-                <div>
-                    <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                        Cliente
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a class="dropdown-item" href="../pages/cliente.php">Exibir</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="../pages/cadastroCliente.php">Cadastrar</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                        Produto
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a class="dropdown-item" href="../pages/produto.php">Exibir</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="../pages/cadastroProduto.php">Cadastrar</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="../pages/cadastroTamanho.php">Tamanho</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                        Venda
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a class="dropdown-item" href="../pages/venda.php">Histórico</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="../pages/cadastroVenda.php">Gerar</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="dropdown-item" href="../pages/cliente.php">Exibir</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="../pages/cadastroCliente.php">Cadastrar</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
+                Produto
+            </button>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="dropdown-item" href="../pages/produto.php">Exibir</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="../pages/cadastroProduto.php">Cadastrar</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="../pages/cadastroTamanho.php">Tamanho</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
+                Venda
+            </button>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="dropdown-item" href="../pages/venda.php">Histórico</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="../pages/cadastroVenda.php">Gerar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
