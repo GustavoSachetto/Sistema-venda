dafa6ebcc89e689d1311081b73a16aeb9cf0dc1f
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-19 17:16:23 -0300

    add

--- view/pages/cadastroTamanho.php
+++ view/pages/cadastroTamanho.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cadastro tamanho</title>
         <?php require ('../layout/head.php') ?>
+        <link rel="stylesheet" href="../css/form.css">
     </head>
     <body>
         <?php 
