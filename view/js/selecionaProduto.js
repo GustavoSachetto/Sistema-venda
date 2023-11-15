8b91359c8cdd773e72b6bc60667216858b28296e
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-15 08:49:18 -0300

    Revert "Update selecionaProduto.js"
    This reverts commit 7dd269eeb90ca3059e5b8f093c7c9cee20190508.

--- view/js/selecionaProduto.js
+++ view/js/selecionaProduto.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+
     let selecionar = $('#slcProduto');
+
     selecionar.change(() => {
         codigoProduto = selecionar.val();
         produtos.forEach(item => {
