4167c26cb8227de83c8cf2d3c8308164fc4b3163
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-17 18:18:12 -0300

    Estilizando a navegação

---
+++ view/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    const btnSidebar = $('.btn-sidebar');
+    const textSwitch = $('.text-switch');
+
+    btnSidebar.click(() => {
+        $('.sidebar').toggleClass('close');
+    });
+
+    $('#tema').click(() => {
+        $('body').toggleClass('dark');
+        if ($('body').attr('class') == 'dark') {
+            textSwitch.text('Modo claro');
+        } else {
+            textSwitch.text('Modo escuro');
+        }
+    }); 
+});
