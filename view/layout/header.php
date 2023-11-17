b9ba2b56652b00ed75c26e9c92e25ad4d66a5dae
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-17 18:39:48 -0300

    Alterações no header

--- view/layout/header.php
+++ view/layout/header.php
@@ -1,75 +1,105 @@
-<header>
-    <h1>Sistema - venda</h1>
-    <button id="btn-menu" class="btn-menu">
-        Menu
-    </button>
-    <nav>
-        <a href="../../index.php" role="button" class="btn btn-lg">Inicio</a>
-        <a href="../pages/sobre.php" role="button" class="btn btn-lg">Sobre</a>
-        <div>
-            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                Cliente
-            </button>
-            <ul class="dropdown-menu">
-                <li>
-                    <a class="dropdown-item" href="../pages/cliente.php">Exibir</a>
+<nav class="sidebar">
+    <header>
+        <div class="container-header">
+            <span class="container-img">
+                <img src="view/img/logo.png" alt="logo">
+            </span>
+            <span class="text container-text">
+                <h4>Sistema-venda</h4>
+                <span>Projeto backend</span>
+            </span>
+            <i class="bx bx-chevron-left btn-sidebar"></i>
+        </div>
+        <div class="container-search">
+            <i class='bx bx-search'></i>
+            <input type="text" placeholder="Buscar...">
+        </div>
+        <nav class="container-nav">
+            <ul>
+                <li class="menu-link">
+                    <i class='bx bx-home-alt'></i>
+                    <div class="text">
+                        <a class="painel-link" href="../../index.html">Painel</a>
+                    </div>
                 </li>
-                <li>
-                    <a class="dropdown-item" href="../pages/cadastroCliente.php">Cadastrar</a>
+                <li class="dropdown">
+                    <div type="button" class="menu-link" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class='bx bx-user'></i>
+                        <div class="text">
+                            <span>Cliente</span>
+                            <i class='bx bx-chevron-down arrow'></i>
+                        </div>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a href="cliente.php">Exibir</a></li>
+                        <li><a href="cadastroCliente.php">Cadastrar</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <div type="button" class="menu-link" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class='bx bx-cart'></i>
+                        <div class="text">
+                            <span>Produto</span>
+                            <i class='bx bx-chevron-down arrow'></i>
+                        </div>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a href="produto.php">Exibir</a></li>
+                        <li><a href="cadastroProduto.php">Cadastrar</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <div type="button" class="menu-link" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class='bx bx-package'></i>
+                        <div class="text">
+                            <span>Estoque</span>
+                            <i class='bx bx-chevron-down arrow'></i>
+                        </div>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a href="estoque.php">Exibir</a></li>
+                        <li><a href="cadastroEstoque.php">Cadastrar</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <div type="button" class="menu-link" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class='bx bx-collapse-alt'></i>
+                        <div class="text">
+                            <span>Tamanho</span>
+                            <i class='bx bx-chevron-down arrow'></i>
+                        </div>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a href="tamanho.php">Exibir</a></li>
+                        <li><a href="cadastroTamanho.php">Cadastrar</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <div type="button" class="menu-link" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class='bx bx-credit-card'></i>
+                        <div class="text">
+                            <span>Venda</span>
+                            <i class='bx bx-chevron-down arrow'></i>
+                        </div>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a href="venda.php">Exibir</a></li>
+                        <li><a href="cadastroVenda.php">Cadastrar</a></li>
+                    </ul>
                 </li>
             </ul>
+        </nav>
+    </header>
+    <div id="tema" class="container-tema">
+        <div class="tema-mode">
+            <i class="bx bx-sun tema-claro"></i>
+            <i class="bx bx-moon tema-escuro"></i>
         </div>
-        <div>
-            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                Produto
-            </button>
-            <ul class="dropdown-menu">
-                <li>
-                    <a class="dropdown-item" href="../pages/produto.php">Exibir</a>
-                </li>
-                <li>
-                    <a class="dropdown-item" href="../pages/cadastroProduto.php">Cadastrar</a>
-                </li>
-            </ul>
+        <div class="container-switch">
+            <span class="text-switch">Modo escuro</span>
+            <div class="toggle-switch">
+                <span id="switch" class="switch">
+            </div>
         </div>
-        <div>
-            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                Estoque
-            </button>
-            <ul class="dropdown-menu">
-                <li>
-                    <a class="dropdown-item" href="../pages/estoque.php">Exibir</a>
-                </li>
-                <li>
-                    <a class="dropdown-item" href="../pages/cadastroEstoque.php">Cadastrar</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                Tamanho
-            </button>
-            <ul class="dropdown-menu">
-                <li>
-                    <a class="dropdown-item" href="../pages/tamanho.php">Exibir</a>
-                </li>
-                <li>
-                    <a class="dropdown-item" href="../pages/cadastroTamanho.php">Cadastrar</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <button type="button" class="btn btn-lg" data-bs-toggle="dropdown" aria-expanded="false">
-                Venda
-            </button>
-            <ul class="dropdown-menu">
-                <li>
-                    <a class="dropdown-item" href="../pages/venda.php">Exibir</a>
-                </li>
-                <li>
-                    <a class="dropdown-item" href="../pages/cadastroVenda.php">Cadastrar</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+    </div>
+</nav>
