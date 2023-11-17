594f10e25d9611c84ca7701d0a2fc6a5a802cba2
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-17 18:37:10 -0300

    add

--- view/layout/head.php
+++ view/layout/head.php
@@ -1,5 +1,8 @@
-<link rel="stylesheet" href="../css/style.css">
+<link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">        
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<script src="../js/responsivo.js" defer></script>
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link rel="stylesheet" href="../css/style.css">
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="../js/script.js"></script>
