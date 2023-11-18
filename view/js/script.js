4b4e7d2cb58c031d0141fa98b0633d2828e55560
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-18 12:00:00 -0300

    Add

--- view/js/script.js
+++ view/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
 
     const btnSidebar = $('.btn-sidebar');
+    const tema = $('#tema');
     const textSwitch = $('.text-switch');
 
     btnSidebar.click(() => {
         $('.sidebar').toggleClass('close');
     });
 
-    $('#tema').click(() => {
+    tema.click(() => {
         $('body').toggleClass('dark');
         if ($('body').attr('class') == 'dark') {
             textSwitch.text('Modo claro');
