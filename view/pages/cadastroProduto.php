aa94c0f9021e874eea983b9da77b4e39d4a77eda
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-18 12:22:56 -0300

    Update cadastroProduto.php

--- view/pages/cadastroProduto.php
+++ view/pages/cadastroProduto.php
@@ -16,6 +16,14 @@
         <main>
             <article>
                 <section>
+                    <div class="container-user">
+                        <i class='bx bxs-user-circle'></i>
+                        <div class="user">
+                            <span>Douglas junior</span>
+                            <span class="type-user">ADMIN</span>
+                        </div>
+                    </div>
+                    <h3>Cliente / produto</h3>
                     <form method="post" action="?codProduto=<?= $codProduto ?>">
                         <h2>Cadastro produto</h2>
                         <fieldset id="produto">
@@ -27,8 +35,13 @@
                             <input type="text" placeholder="Tipo" name="txtTipo" minlength="4" maxlength="30" required>
                             <input type="text" placeholder="Marca" name="txtMarca" minlength="4" maxlength="30" required>
                         </fieldset>
-                        <button type="submit" name="envProduto">Enviar</button>
+                        <hr>
+                        <div class="container-button">
+                            <button type="reset"><i class='bx bx-trash'></i> <span>Limpar</span></button>
+                            <button type="submit"><i class='bx bx-message-square-add'></i> <span>Cadastrar</span></button>
+                        </div>
                     </form>
+                    <h3>Cliente / tamanho</h3>
                     <form method="post" action="">
                         <h2>Cadastro tamanho</h2>
                         <fieldset>
@@ -38,7 +51,11 @@
                             </select>
                             <input type="number" placeholder="Quantidade" name="txtQuantidade" required>
                         </fieldset>
-                        <button type="submit" name="envTamanho">Enviar</button>
+                        <hr>
+                        <div class="container-button">
+                            <button type="reset"><i class='bx bx-trash'></i> <span>Limpar</span></button>
+                            <button type="submit"><i class='bx bx-message-square-add'></i> <span>Cadastrar</span></button>
+                        </div>
                     </form>
                 </section>
             </article>
