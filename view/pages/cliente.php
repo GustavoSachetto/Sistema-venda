3cc385c15293f1fd66cf3a6e16840408ffa43209
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-20 13:34:09 -0300

    Update cliente.php

--- view/pages/cliente.php
+++ view/pages/cliente.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Exibir clientes</title>
+        <title>Sistema-venda | exibe clientes</title>
         <?php require ('../layout/head.php') ?>
         <link rel="stylesheet" href="../css/table.css">
     </head>
@@ -15,8 +15,19 @@
         ?>
         <main>
             <article>
-                <section>
+                <section id="user">
+                    <div class="container-user">
+                        <i class='bx bxs-user-circle'></i>
+                        <div class="user">
+                            <span>Douglas junior</span>
+                            <span class="type-user">ADMIN</span>
+                        </div>
+                    </div>
+                </section>
+                <section id="busca" class="container-busca">
+                    <h3>Cliente / exibe</h3>
                     <form method="post" action="">
+                        <h2>Busca clientes</h2>
                         <fieldset>
                             <input type="text" placeholder="Nome" name="txtNome" maxlength="80">
                             <input type="text" placeholder="CPF" name="txtCPF" maxlength="11">
@@ -25,11 +36,14 @@
                             <input type="text" placeholder="Cidade" name="txtCidade" maxlength="30">    
                             <input type="text" placeholder="Bairro" name="txtBairro" maxlength="30">
                             <input type="text" placeholder="Codigo cliente" name="txtCod">
-                            <button type="submit" name="envBusca">Buscar</button>
+                            <button type="submit" name="envBusca">
+                                <i class='bx bx-search'></i>
+                                <span>Buscar</span>
+                            </button>
                         </fieldset>
                     </form>
                 </section>
-                <section>
+                <section id="table" class="container-table">
                     <?php 
                         include ('../../controller/exibeCliente.php'); 
                         include ('../../controller/alteracaoCliente.php');
