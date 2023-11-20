7667d1baf6ae0e65a23fd51c2dafd1d0d25bae46
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-20 14:04:37 -0300

    Update exibeProduto.php

--- controller/exibeProduto.php
+++ controller/exibeProduto.php
@@ -8,37 +8,49 @@
     
     if ($verifica[0]['count(*)'] !== 0) {
 
-        echo "<form method='post' action=''>
-        <table>
-            <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>Categoria</td>
-                <td>Genero</td>
-                <td>Marca</td>
-                <td>Tipo</td>
-                <td>Valor UN</td>
-                <td>Ações</td>
-            </tr>
+        echo "
+        <form method='post' action=''>
+            <h2>Exibe estoques</h2>
+            <table>
+                <thead>
+                    <td>#</td>
+                    <td>Nome</td>
+                    <td>Categoria</td>
+                    <td>Genero</td>
+                    <td>Marca</td>
+                    <td>Tipo</td>
+                    <td>Valor UN</td>
+                    <td>Ações</td>
+                </tr>
+            </thead>
+            <tbody>
         ";
         foreach ($produtos as $produto) {
 
             echo "
-                <tr>
-                    <td>" . stripslashes($produto['codProduto']) . "</td>
-                    <td>" . stripslashes($produto['nomeProduto']) . "</td>
-                    <td>" . stripslashes($produto['categoria']) . "</td>
-                    <td>" . stripslashes($produto['genero']) . "</td>
-                    <td>" . stripslashes($produto['marca']) . "</td>
-                    <td>" . stripslashes($produto['tipo']) . "</td>
-                    <td>" . "R$ " . number_format($produto['valor'], 2, ",", ".") . "</td>
-                    <td>
-                        <button type='submit' name='editar' value=" . $produto['codProduto'] . ">Editar</button>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>" . stripslashes($produto['codProduto']) . "</td>
+                        <td>" . stripslashes($produto['nomeProduto']) . "</td>
+                        <td>" . stripslashes($produto['categoria']) . "</td>
+                        <td>" . stripslashes($produto['genero']) . "</td>
+                        <td>" . stripslashes($produto['marca']) . "</td>
+                        <td>" . stripslashes($produto['tipo']) . "</td>
+                        <td>" . "R$ " . number_format($produto['valor'], 2, ",", ".") . "</td>
+                        <td>
+                            <button type='submit' name='editar' value=" . $produto['codProduto'] . ">Editar</button>
+                        </td>
+                    </tr>
             ";
         }
-        echo "</table></form>";
+        echo "
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan='13'>Cadastrar mais produtos</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </form>";
     } else {
         echo "<p>Nenhum produto cadastrado!</p>";
     }
