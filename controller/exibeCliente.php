efb24e6350b5d89efc809b2b23d79adfcc9be0de
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-20 10:33:51 -0300

    Update exibeCliente.php

--- controller/exibeCliente.php
+++ controller/exibeCliente.php
@@ -10,41 +10,45 @@
         
         echo "<form method='post' action=''>
         <table>
-            <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>CPF</td>
-                <td>CEP</td>
-                <td>Rua</td>
-                <td>Número</td>
-                <td>Bairro</td>
-                <td>Cidade</td>
-                <td>UF</td>
-                <td>Tipo Logradouro</td>
-                <td>Complemento</td>
-                <td>Observação</td>
-                <td>Ações</td>
-            </tr>
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Nome</td>
+                    <td>CPF</td>
+                    <td>CEP</td>
+                    <td>Rua</td>
+                    <td>Número</td>
+                    <td>Bairro</td>
+                    <td>Cidade</td>
+                    <td>UF</td>
+                    <td>Tipo Logradouro</td>
+                    <td>Complemento</td>
+                    <td>Observação</td>
+                    <td>Ações</td>
+                </tr>
+            </thead>
         ";
         foreach ($clientes as $cliente) {
             echo "
-                <tr>
-                    <td>" . stripslashes($cliente['codCliente']) . "</td>
-                    <td>" . stripslashes($cliente['nomeCliente']) . "</td>
-                    <td>" . stripslashes($cliente['cpf']) . "</td>
-                    <td>" . stripslashes($cliente['CEP']) . "</td>
-                    <td>" . stripslashes($cliente['rua']) . "</td>
-                    <td>" . stripslashes($cliente['nResidencial']). "</td>
-                    <td>" . stripslashes($cliente['bairro']) . "</td>
-                    <td>" . stripslashes($cliente['cidade']) . "</td>
-                    <td>" . stripslashes($cliente['UF']) . "</td>
-                    <td>" . stripslashes($cliente['tipoLogradouro']) . "</td>
-                    <td>" . stripslashes($cliente['complemento']) . "</td>
-                    <td>" . stripslashes($cliente['observacao']) . "</td>
-                    <td>
-                    <button type='submit' name='excluir' value=" . $cliente['codCliente'] . ">Excluir</button>
-                </td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td>" . stripslashes($cliente['codCliente']) . "</td>
+                        <td>" . stripslashes($cliente['nomeCliente']) . "</td>
+                        <td>" . stripslashes($cliente['cpf']) . "</td>
+                        <td>" . stripslashes($cliente['CEP']) . "</td>
+                        <td>" . stripslashes($cliente['rua']) . "</td>
+                        <td>" . stripslashes($cliente['nResidencial']). "</td>
+                        <td>" . stripslashes($cliente['bairro']) . "</td>
+                        <td>" . stripslashes($cliente['cidade']) . "</td>
+                        <td>" . stripslashes($cliente['UF']) . "</td>
+                        <td>" . stripslashes($cliente['tipoLogradouro']) . "</td>
+                        <td>" . stripslashes($cliente['complemento']) . "</td>
+                        <td>" . stripslashes($cliente['observacao']) . "</td>
+                        <td>
+                            <button type='submit' name='excluir' value=" . $cliente['codCliente'] . ">Excluir</button>
+                        </td>
+                    </tr>
+                </tbody>
             ";
         }
         echo "</table></form>";
