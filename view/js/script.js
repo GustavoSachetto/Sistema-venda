6cac99d650b0bed8173e1a7bfee4313d308efea7
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-21 17:38:16 -0300

    Update script.js

--- view/js/script.js
+++ view/js/script.js
@@ -1,19 +1,36 @@
 $(document).ready(function () {
-    // Sidebar
     const btnSidebar = $('.btn-sidebar');
     const tema = $('#tema');
     const textSwitch = $('.text-switch');
-
+    
+    // Sidebar modo
     btnSidebar.click(() => {
         $('.sidebar').toggleClass('close');
     });
 
+    // Tema modo
     tema.click(() => {
+        var theme; 
         $('body').toggleClass('dark');
+        
         if ($('body').attr('class') == 'dark') {
             textSwitch.text('Modo claro');
+            theme = 'DARK';
         } else {
             textSwitch.text('Modo escuro');
+            theme = 'DEFAULT';
         }
+
+        localStorage.setItem('pageTheme', theme);
     }); 
+    
+    let getTheme = localStorage.getItem('pageTheme');
+    
+    if (getTheme === 'DARK') {
+        $('body').addClass('dark');
+        textSwitch.text('Modo claro');
+    } else {
+        $('body').removeClass('dark');
+        textSwitch.text('Modo escuro');
+    }
 });
