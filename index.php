4fb8325e9783f78cedc1746bd16d00ef02e56b41
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-30 20:20:35 -0300

    Iniciando painel e venda.

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Inicio</title>
+        <title>Sistema-venda | Início painel</title>
         
         <link rel="shortcut icon" href="view/img/logo.png" type="image/x-icon">        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link rel="stylesheet" href="view/css/style.css">
+        <link rel="stylesheet" href="view/css/painel.css">
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
@@ -134,7 +135,39 @@
         </nav>
       	<main>
             <article>
-                
+            <section id="user">
+                    <div class="container-user">
+                        <i class='bx bxs-user-circle'></i>
+                        <div class="user">
+                            <span>Douglas junior</span>
+                            <span class="type-user">ADMIN</span>
+                        </div>
+                    </div>
+                </section>
+                <section id="local">
+                    <h3>Início / painel</h3>
+                </section>
+                <section id="information" class="information">
+                    <h2>Informações gerais</h2>
+                    <div class="container-information">
+                        <div id="venda" class="container-venda">
+                            <legend>Venda total</legend>
+                            <h2>R$ 1469,73</h2>
+                        </div>
+                        <div id="estoque" class="container-estoque">
+                            <legend>Estoque total</legend>
+                            <h2>R$ 2458,20</h2>
+                        </div>
+                        <div id="cliente" class="container-cliente">
+                            <legend>N° Clientes</legend>
+                            <h2>10</h2>
+                        </div>
+                        <div id="produto" class="container-produto">
+                            <legend>N° Produtos</legend>
+                            <h2>33</h2>
+                        </div>
+                    </section>
+                    </div>
             </article>
         </main>
         <footer>
