441a5609a0ac5e344b5eec4264e655b0931c41d5
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-05 13:22:09 -0300

    Update alteraEstoque.php

--- controller/alteraEstoque.php
+++ controller/alteraEstoque.php
@@ -15,20 +15,24 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         if (isset($_POST['editar'])) {
-            $codEstoque = $_SESSION['codEstoque'] = $_POST['editar'];
+            $codEstoque = $_SESSION['codEstoque'] = explode("-", $_POST['editar'])[0];
+            $codProduto = $_SESSION['codProduto'] = explode("-", $_POST['editar'])[1];
         }
         
         if (isset($_POST['atualizar'])) {
             $cadTam= addslashes($_POST['slcTamanho']);
             $cadQuantidade= addslashes($_POST['txtQuantidade']);
             $codEstoque = $_SESSION['codEstoque'];
+            $codProduto = $_SESSION['codProduto'];
             
-            $resultadoEditar = $conexao -> atualizaEstoque($cadQuantidade, $cadTam, $codEstoque);
+            $resultadoEditar = $conexao -> atualizaEstoque($cadQuantidade, $cadTam, $codEstoque, $codProduto);
 
-            if ($resultadoEditar === true) {
-                alerta("Concluído!", "Estoque editado com sucesso.", "success");
-            } else {
+            if ($resultadoEditar === 2) {
                 alerta("Erro!", "Estoque já incluido em uma venda.", "error");
+            } else if ($resultadoEditar === 1) {
+                alerta("Erro!", "Estoque já existente no tamanho desejado.", "error");
+            } else {
+                alerta("Concluído!", "Estoque editado com sucesso.", "success");
             }
         }
         
