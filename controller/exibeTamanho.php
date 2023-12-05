82556d69605ed43af3b8741ff2606186e068a758
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-05 13:22:23 -0300

    Update exibeTamanho.php

--- controller/exibeTamanho.php
+++ controller/exibeTamanho.php
@@ -6,7 +6,7 @@
     $consulta = "SELECT count(*) FROM tamanho";
     $verifica = $conexao -> consultaBanco($consulta);
 
-    if ($verifica[0]['count(*)'] !== 0) {
+    if ($verifica[0]['count(*)'] > 0) {
         $tamanhos = $conexao -> consultaTamanho();
 
         echo "
