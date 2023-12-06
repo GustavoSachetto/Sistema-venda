f4c274e916e5c32864d3c4edcf95ea361a998db2
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-06 17:08:32 -0300

    Update recebeCliente.php

--- controller/recebeCliente.php
+++ controller/recebeCliente.php
@@ -3,6 +3,7 @@
     require_once '../../model/conexao.php';
     $config = parse_ini_file('../../model/config.ini');
     $conexao = new conexao ($config['dbname'], $config['host'], $config['user'], $config['password']);
+    date_default_timezone_set('America/Sao_Paulo');
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $cadNome = addslashes($_POST['txtNome']);
@@ -16,13 +17,17 @@
         $cadNumero = addslashes($_POST['txtNumero']);
         $cadComplemento = addslashes($_POST['txtComplemento']);
         $cadObservacao = addslashes($_POST['txtObservacao']);
+        $cadDate = date('Y-m-d');
+        $cadTime = date('H:i:s');
 
         $resultadoCadastro = $conexao -> insereCliente($cadNome, $cadCpf, $cadCep, $cadUF, $cadCidade, $cadBairro, $cadRua, $cadLogradouro, $cadNumero, $cadComplemento, $cadObservacao);
 
         if ($resultadoCadastro === true) {
             alerta("Concluído!", "Cliente cadastrado com sucesso.", "success");
+            $conexao -> insereRegistro("Novo cliente cadastrado.", $cadTime, $cadDate);
         } else {
             alerta("Erro!", "Cliente já existente (verifique o CPF).", "error");
+            $conexao -> insereRegistro("Tentativa do cadastro de cliente.", $cadTime, $cadDate);
         }
     }
 ?> 
