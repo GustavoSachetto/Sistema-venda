c345b28af663ee8afe4d50858f0c4b5f024b2a95
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-06 17:09:13 -0300

    Delete painel.js

--- view/js/painel.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    let notificacao = $('.notificacao');
-    notificacao.mouseenter(() => {
-    $('p > i').attr('class', 'bx bx-trash');
-    $('p > i').css('background', 'var(--button-color-reset)');
-   });
-});
