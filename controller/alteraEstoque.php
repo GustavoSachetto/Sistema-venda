273a9c12c73139694bc99417a47a0ede87dfa3dd
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-06 17:08:14 -0300

    Update alteraEstoque.php

--- controller/alteraEstoque.php
+++ controller/alteraEstoque.php
@@ -3,6 +3,7 @@
     require_once '../../model/conexao.php';
     $config = parse_ini_file('../../model/config.ini');
     $conexao = new conexao ($config['dbname'], $config['host'], $config['user'], $config['password']);
+    date_default_timezone_set('America/Sao_Paulo');
 
     $tamanhos = $conexao -> consultaTamanho();
     
@@ -24,15 +25,20 @@
             $cadQuantidade= addslashes($_POST['txtQuantidade']);
             $codEstoque = $_SESSION['codEstoque'];
             $codProduto = $_SESSION['codProduto'];
+            $cadDate = date('Y-m-d');
+            $cadTime = date('H:i:s');
             
             $resultadoEditar = $conexao -> atualizaEstoque($cadQuantidade, $cadTam, $codEstoque, $codProduto);
 
             if ($resultadoEditar === 2) {
                 alerta("Erro!", "Estoque já incluido em uma venda.", "error");
+                $conexao -> insereRegistro("Tentativa da alteração de estoque.", $cadTime, $cadDate);
             } elseif ($resultadoEditar === 1) {
                 alerta("Erro!", "Estoque já existente no tamanho desejado.", "error");
+                $conexao -> insereRegistro("Tentativa da alteração de estoque.", $cadTime, $cadDate);
             } else {
                 alerta("Concluído!", "Estoque editado com sucesso.", "success");
+                $conexao -> insereRegistro("Alteração de estoque.", $cadTime, $cadDate);
             }
         }
         
