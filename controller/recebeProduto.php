72825efb681000126713ee7242d85e6759b97175
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-06 17:08:38 -0300

    Update recebeProduto.php

--- controller/recebeProduto.php
+++ controller/recebeProduto.php
@@ -3,6 +3,7 @@
     require_once '../../model/conexao.php';
     $config = parse_ini_file('../../model/config.ini');
     $conexao = new conexao ($config['dbname'], $config['host'], $config['user'], $config['password']);
+    date_default_timezone_set('America/Sao_Paulo');
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $cadNomeP = addslashes($_POST['txtNomeP']);
@@ -12,13 +13,17 @@
         $cadGen = addslashes($_POST['txtGen']);
         $cadTipo = addslashes($_POST['txtTipo']);
         $cadMarca = addslashes($_POST['txtMarca']);
+        $cadDate = date('Y-m-d');
+        $cadTime = date('H:i:s');
 
         $resultadoCadastro = $conexao -> insereProduto($cadNomeP, $cadValorC, $cadValorV, $cadCat, $cadGen, $cadTipo, $cadMarca);
         
         if ($resultadoCadastro === true) {
             alerta("Concluído!", "Produto cadastrado com sucesso.", "success");
+            $conexao -> insereRegistro("Novo produto cadastrado.", $cadTime, $cadDate);
         } else {
             alerta("Erro!", "Produto já existente.", "error");
+            $conexao -> insereRegistro("Tentativa do cadastro de produto.", $cadTime, $cadDate);
         }
     }    
 ?>
