abcb246388e6fd6527dd546b9797f173c70bb1b0
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-06 18:00:48 -0300

    Update DML - Exclusao e alteracao.sql

--- DOCS/sql/DML - Exclusao e alteracao.sql
+++ DOCS/sql/DML - Exclusao e alteracao.sql
@@ -17,7 +17,7 @@ update venda set dataHora = "2023-04-06 14:56:28" where codCliente = 9;
 update tamanho set tipoTamanho = "XXG" where codTam = 6;
 
 delete from vendaitem where codVenda = 17;
-delete from tamanhoP where codTam = 5;
+delete from tamanhop where codTam = 5;
 delete from produto where codProduto = 1;
 
 UPDATE cliente SET nomeCliente = 'Gustavo Sachetto', cpf = 12345678911, tipoLogradouro = 'Rua', bairro = 'Parque paulista', rua = 'Damasco', UF = 'SP', nResidencial = 288, cidade = 'Franco da rocha', CEP = '07855080', complemento = '', observacao = '' WHERE codCliente = 11;
